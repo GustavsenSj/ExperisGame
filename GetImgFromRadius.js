381ef29457400dc4d062e4aa2e6717f9d1528329
Sjur G <sjur.gustavsen@no.experis.com>
2023-11-23 09:33:20 +0100

    firebase deploy

--- GetImgFromRadius.js
+++ GetImgFromRadius.js
@@ -1,15 +1,27 @@
 export function GetImgFromRadius(radius) {
-    const orbTypes = ["Html", "Css", "Js", "TS", "ang", "React", "Java", "net", "Db", "AZ", "ex"];
-    const baseRadius = 20;
-    const increment = 15;
-    // Calculate the index based on the base radius and increment
-    // index = (radius-baseRadius)/increment
-    const index = Math.floor((radius - baseRadius) / increment);
+  const orbTypes = [
+    "Html",
+    "Css",
+    "Js",
+    "TS",
+    "Ang",
+    "React",
+    "Java",
+    "Net",
+    "Db",
+    "AZ",
+    "Ex",
+  ];
+  const baseRadius = 20;
+  const increment = 15;
+  // Calculate the index based on the base radius and increment
+  // index = (radius-baseRadius)/increment
+  const index = Math.floor((radius - baseRadius) / increment);
 
-    // Check if the index is within the valid range
-    if (index >= 0 && index < orbTypes.length) {
-        return `./assets/orbs/${orbTypes[index]}.png`;
-    }
+  // Check if the index is within the valid range
+  if (index >= 0 && index < orbTypes.length) {
+    return `./assets/orbs/${orbTypes[index]}.png`;
+  }
 
-    // Handle the case where the radius doesn't match any orb type
+  // Handle the case where the radius doesn't match any orb type
 }
